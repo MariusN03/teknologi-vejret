f21b127c72b6a52b2b0495c40b18e317ab95f38b
brofus1 <sofusba@gmail.com>
2022-04-21 11:18:02 +0200

    app points added

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 let client
 let info
 let id
-let points = 50
+let amount
+let points = 25
 
 function setup(){
+    amount = select('#points')
     let params = getURLParams()
     id = params.id
     info = select('#info')
@@ -13,13 +15,50 @@ function setup(){
         console.log(response);
         console.log('<b>Connected to nextblablabla.dk</b>')
 
-        client.subscribe('programmering')
+        client.subscribe('point')
 
         client.on('message', (topic, message) => {
             info.html('<b>Modtog besked:</b> ' + message + ' <b>på emnet:</b> ' + topic)
+            if (topic == "point"){
+                points = points + message
+                console.log(message);
+            }
         })
     })
+    
+    minus = () => {
+        points = points - 10
+    }
+
+    plus = () => {
+        points = points + 10
+    }
 }
 
 function draw(){
+    if (points <= 10){
+        document.getElementById('img-container').style.backgroundImage = "url('./assets/flower1.png')"
+    }
+    else if(points > 10 && points <= 20){
+        document.getElementById('img-container').style.backgroundImage = "url('./assets/flower2.png')"
+    }
+    else if(points > 20 && points <= 30){
+        document.getElementById('img-container').style.backgroundImage = "url('./assets/flower3.png')"
+    }
+    else if(points > 30 && points <= 40){
+        document.getElementById('img-container').style.backgroundImage = "url('./assets/flower4.png')"
+    }
+    else if(points > 40 && points <= 50){
+        document.getElementById('img-container').style.backgroundImage = "url('./assets/flower5.png')"
+    }
+
+    else if (points > 50) {
+        points = 50
+    }
+    
+    if (points < 0){
+        points = 0
+    }
+
+    amount.html('Du har ' + points + ' points')
 }
